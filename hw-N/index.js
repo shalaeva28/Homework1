10e6823751ebb3ce846c154f4465de81f143951d
shalaeva28 <nastenkashalaeva@bk.ru>
2023-04-08 02:14:25 +0300

    js tasks

---
+++ hw-N/index.js
@@ -0,0 +1,47 @@
+//alert("Привет Мир");
+
+let firstTask=20;
+alert(firstTask);
+
+let secondTask=2007;
+alert(secondTask); 
+
+let name = "Брендан Айк";
+alert(name);
+
+let x=10;
+let y=2;
+alert(x+y);
+alert(x-y);
+alert(x*y);
+alert(x/y);
+
+let result= 2**5;
+alert(result);
+
+let a=9;
+let b=2;
+alert(a%b);
+
+let num=1;
+num +=5;
+num -=3;
+num *=7;
+num /=3;
+num++;
+num --;
+alert(num);
+
+let age=prompt("Сколько вам лет?");
+alert(age);
+
+const user={
+    name:"Лол",weight:"19",isAdmin:"true"
+}
+user.cityOfResidence=false;
+delete user.cityOfResidence;
+let info = prompt("Какую информацию хотите узнать о пользователе?",);
+alert (user.info);
+
+let userName = prompt("Как вас зовут?");
+alert(`Привет,${userName}!`);
