dea84f8f41574ff6ed140b83b5845fbc23b50fa7
shalaeva28 <nastenkashalaeva@bk.ru>
2023-04-12 03:09:50 +0300

    add changes

--- hw-N/index.js
+++ hw-N/index.js
@@ -1,6 +1,6 @@
 //alert("Привет Мир");
 
-let firstTask=20;
+/*let firstTask=20;
 alert(firstTask);
 
 let secondTask=2007;
@@ -44,4 +44,74 @@ let info = prompt("Какую информацию хотите узнать о 
 alert (user.info);
 
 let userName = prompt("Как вас зовут?");
-alert(`Привет,${userName}!`);
+alert(`Привет,${userName}!`); */
+
+let password="Asssasin";
+let text=prompt("Пароль введен верно");
+if (text=password) {
+    console.log ('Пароль введен верно');
+    
+} else {
+    console.log ('Пароль введен неправильно');
+}
+
+let c=2;
+if (c>0 && c<10) {
+    console.log("Верно");
+} else {
+    console.log("Неверно");
+}
+
+let d=23;
+let e=114;
+if (d>100 || e>100) {
+    console.log("Верно");
+} else {
+    console.log("Неверно");
+}
+
+let a=2;
+let b=3;
+alert(a+b);
+
+let monthNumber=prompt("Введите номер месяца");
+switch (monthNumber) {
+    case 1:
+        console.log ('зима');
+        break;
+    case 2:
+        console.log ('зима');
+        break;
+    case 3:
+        console.log ('весна');
+        break;
+    case 4:
+        console.log ('весна');
+        break;
+    case 5:
+        console.log ('весна');
+        break;
+    case 6:
+        console.log ('лето');
+        break;
+    case 7:
+        console.log ('лето');
+        break;
+    case 8:
+        console.log ('лето');
+        break;
+    case 9:
+        console.log ('осень');
+        break;
+    case 10:
+        console.log ('осень');
+        break; 
+    case 11:
+        console.log ('осень');
+        break;       
+    case 12:
+        console.log ('зима');
+        break;                                                              
+    default: console.log('Нет такого месяца');
+        break;
+}
